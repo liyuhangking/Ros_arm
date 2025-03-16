553760b5f2554b54c368bbe86bbfeed27983cb0e
liyuhangking <1913605045@qq.com>
2025-03-16 12:23:20 +0800

    robot_model

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/liyuhang/Ros_arm/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/liyuhang/Ros_arm/devel;/opt/ros/noetic'.split(';') if '/home/liyuhang/Ros_arm/devel;/opt/ros/noetic' != '' else []
---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/liyuhang/Ros_arm/src/my_robot_description
+# Build directory: /home/liyuhang/Ros_arm/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
